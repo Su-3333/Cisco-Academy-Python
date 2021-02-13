aafd422db51ea69936c0bd57ae6896d0314e3237
Suh <hasuh@hotmail.co.uk>
2021-02-13 13:49:34 +0000

    Succesful use of parentheses when using operators

---
+++ Lab 2.1.6.10.py
@@ -0,0 +1,14 @@
+# Objectives
+# becoming familiar with the concept of numbers, operators and arithmetic operations in Python;
+# understanding the precedence and associativity of Python operators, as well as the proper use of parentheses.
+
+
+x = float(input("Enter value for x: "))
+
+
+y = (1/(x+(1/(x+(1/(x+1/x))))))
+# put your code here
+
+print("y =", y)
+
+# completed task succesfully
