c395e4550b828ff195db73fcba968238bd88fb36
Suh <hasuh@hotmail.co.uk>
2021-02-13 12:59:45 +0000

    Add inputs, output and simple operatives

---
+++ Lab 2.1.6.9.py
@@ -0,0 +1,18 @@
+# input a float value for variable a here
+# input a float value for variable b here
+
+# output the result of addition here
+# output the result of subtraction here
+# output the result of multiplication here
+# output the result of division here
+
+
+a = float(input())
+b = float(input())
+
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+
+print("\nThat's all, folks!")
