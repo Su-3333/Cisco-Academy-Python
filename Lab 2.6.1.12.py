a786e56b521e5b76820bc10b8bc33ab891921428
Suh <hasuh@hotmail.co.uk>
2021-03-07 14:04:09 +0000

    commit new version of cisco academy

---
+++ Lab 2.6.1.12.py
@@ -0,0 +1,21 @@
+# name = input("Enter your name: ")
+# print("Hello, " + name + ". Nice to meet you!")
+#
+# print("\nPress Enter to end the program.")
+# input()
+# print("THE END.")
+
+# num_1 = input("Enter the first number: ") # Enter 12, input is a string
+# num_2 = input("Enter the second number: ") # Enter 21, input is a string
+#
+# print(num_1 + num_2) # the program returns 1221 as a string
+
+# my_input = input("Enter something: ") # Example input: hello
+# print(my_input * 3) # Expected output: hellohellohello
+
+# x = int(input("Enter a number: ")) # The user enters 2
+# print(x * "5") # output is 55
+
+# x = input("Enter a number: ") # The user enters 2
+# print(type(x)) # class 'str'
+
