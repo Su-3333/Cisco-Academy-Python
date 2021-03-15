51d3a2c6db60cfcfcdcb12b2783c3ff3224ae892
Suh <hasuh@hotmail.co.uk>
2021-03-15 21:18:11 +0000

    loops

---
+++ Lab 3.2.1.6.py
@@ -0,0 +1,10 @@
+import time
+
+for i in range(1, 6):
+    print(i, "Mississippi")
+    time.sleep(1)
+# Write a for loop that counts to five.
+# Body of the loop - print the loop iteration number and the word "Mississippi".
+# Body of the loop - use: time.sleep(1)
+print("Ready or not, here I come!")
+# Write a print function with the final message.
